82fce5feee9a191aa8a125bf9bd19ea7a19ca99f
Olusegun Osikoya <52752528+olugbon@users.noreply.github.com>
2025-08-30 11:49:15 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from container!' });
+});
+
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`);
+});
